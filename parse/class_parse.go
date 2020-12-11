fbe3c1c77363fb8c4952c166e65ed4503f49348e
web1992 <554933654@qq.com>
2020-12-11 17:26:14 +0800

    add file parse

---
+++ parse/class_parse.go
@@ -0,0 +1,14 @@
+package parse
+
+import core "class-parse/core"
+
+type ClassParse struct {
+	fileName string
+}
+
+func (cp ClassParse) parseFile(fileName string) (core.ClassFile, error) {
+
+	var classFile core.ClassFile
+
+	return classFile, nil
+}
