0fc51300fcb1cba704b7f1a93031454ffae113e0
web1992 <554933654@qq.com>
2020-12-31 00:31:02 +0800

    fix class comment

--- core/class.go
+++ core/class.go
@@ -1,29 +1,5 @@
 package core
 
-/**
-ClassFile {
-    u4             magic;
-    u2             minor_version;
-    u2             major_version;
-    u2             constant_pool_count;
-    cp_info        constant_pool[constant_pool_count-1];
-    u2             access_flags;
-    u2             this_class;
-    u2             super_class;
-    u2             interfaces_count;
-    u2             interfaces[interfaces_count];
-    u2             fields_count;
-    field_info     fields[fields_count];
-    u2             methods_count;
-    method_info    methods[methods_count];
-    u2             attributes_count;
-    attribute_info attributes[attributes_count];
-}
-*/
-
-// U1_L 1 byte
-// U2_L 2 bytes
-// U4_L 4 bytes
 const (
 	U1_L = 1 // 1 byte
 	U2_L = 2 // 2 bytes
@@ -55,6 +31,26 @@ type ReferenceIndex int
 type BootstrapMethodAttrIndex int
 type String string
 
+/**
+ClassFile {
+    u4             magic;
+    u2             minor_version;
+    u2             major_version;
+    u2             constant_pool_count;
+    cp_info        constant_pool[constant_pool_count-1];
+    u2             access_flags;
+    u2             this_class;
+    u2             super_class;
+    u2             interfaces_count;
+    u2             interfaces[interfaces_count];
+    u2             fields_count;
+    field_info     fields[fields_count];
+    u2             methods_count;
+    method_info    methods[methods_count];
+    u2             attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
 // ClassFile
 type ClassFile struct {
 	Magic
